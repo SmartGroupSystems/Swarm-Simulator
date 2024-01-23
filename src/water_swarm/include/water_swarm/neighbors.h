bbc8c4d3b08b1a341b0e61037498bf23685c1a7b
Ruocheng Li <1598863621@qq.com>
2024-01-23 20:56:36 +0800

    update neighbors

--- src/water_swarm/include/water_swarm/neighbors.h
+++ src/water_swarm/include/water_swarm/neighbors.h
@@ -4,27 +4,25 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/topic_manager.h>
+#include <std_msgs/String.h>
 #include <regex>
 #include <map>
+
+#include "water_swarm/Odom.h"
+#include "water_swarm/OdomWithNeighbors.h"
+#include "water_swarm/OdomBroadcast.h"
 
 void subscribeOdom(const std::string &topic_name, ros::NodeHandle &nh);
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg, const std::string& uav_name);
 std::map<std::string, ros::Subscriber> subscribers;
+std::map<std::string, ros::Publisher>  odom_neighbors_publishers;
+ros::Publisher odomBroadcast_pub;
 
 double neighbor_dist_;
+double threshold_dist_;
 
-struct Position {
-    double x, y, z;
-};
+std::vector<water_swarm::Odom> broadcast_odom;
 
-struct Velocity {
-    double x, y, z;
-};
-
-struct Odom {
-    Position position;
-    Velocity velocity;
-};
 
 
 #endif
--- src/water_swarm/src/neighbors.cpp
+++ src/water_swarm/src/neighbors.cpp
@@ -1,10 +1,60 @@
 #include "neighbors.h"
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg, const std::string& uav_name) {
-    // 处理odom消息
-    ROS_INFO("Received odom from %s", uav_name.c_str());
-    // 你可以在这里添加更多的逻辑
-    
+    // ROS_INFO("Received odom from %s", uav_name.c_str());
+    water_swarm::Odom current_odom;
+    current_odom.position.x = msg->pose.pose.position.x;
+    current_odom.position.y = msg->pose.pose.position.y;
+    current_odom.position.z = msg->pose.pose.position.z;
+    current_odom.velocity.x = msg->twist.twist.linear.x;
+    current_odom.velocity.y = msg->twist.twist.linear.y;
+    current_odom.velocity.z = msg->twist.twist.linear.z;
+
+    // 检查broadcast_odom是否为空
+    if (broadcast_odom.empty()) {
+        broadcast_odom.push_back(current_odom);
+    } else {
+        // 检查距离并更新broadcast_odom
+        for (auto it = broadcast_odom.begin(); it != broadcast_odom.end(); ) {
+            double dist = sqrt(pow(it->position.x - current_odom.position.x, 2) +
+                               pow(it->position.y - current_odom.position.y, 2) +
+                               pow(it->position.z - current_odom.position.z, 2));
+            if (dist < threshold_dist_) {
+                it = broadcast_odom.erase(it);
+                break;
+            } else {
+                ++it;
+            }
+        }
+        broadcast_odom.push_back(current_odom);
+    }
+    water_swarm::OdomBroadcast odom_broadcast_msg;
+    odom_broadcast_msg.header.frame_id  = "world";
+    odom_broadcast_msg.header.stamp     = ros::Time::now();
+    odom_broadcast_msg.OdomBroadcast    = broadcast_odom;
+    // ROS_INFO("Size of broadcast_odom: %lu", broadcast_odom.size());
+
+    //find neighbors
+    std::vector<water_swarm::Odom> neighbors;
+    for (const auto& neighbor_odom : broadcast_odom) {
+        double dist = sqrt(pow(neighbor_odom.position.x - current_odom.position.x, 2) +
+                           pow(neighbor_odom.position.y - current_odom.position.y, 2) +
+                           pow(neighbor_odom.position.z - current_odom.position.z, 2));
+        if (dist < neighbor_dist_ && dist != 0) {  // dist != 0 确保不将当前odom作为其自己的邻居
+            neighbors.push_back(neighbor_odom);
+        }
+    }
+
+    // 创建OdomWithNeighbors消息
+    water_swarm::OdomWithNeighbors odom_with_neighbors_msg;
+    odom_with_neighbors_msg.header.stamp    = ros::Time::now();
+    odom_with_neighbors_msg.header.frame_id = "world"; // 或者其他适合的frame_id
+    odom_with_neighbors_msg.myOdom = current_odom;
+    odom_with_neighbors_msg.neighborsOdom = neighbors;
+
+    //pub odom_with_neighbors  odom_broadcast
+    odom_neighbors_publishers[uav_name].publish(odom_with_neighbors_msg);
+    odomBroadcast_pub.publish(odom_broadcast_msg);
 }
 
 void subscribeOdom(const std::string &topic_name, ros::NodeHandle &nh) {
@@ -19,7 +69,8 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "neighbors_node");
     ros::NodeHandle nh("~");
 
-    nh.param("neighbor_dist",neighbor_dist_,1.0);
+    nh.param("neighbor_dist",   neighbor_dist_, 1.0);
+    nh.param("threshold_dist",  threshold_dist_,0.1);
 
     ROS_INFO("Neighbors node has started.");
 
@@ -33,9 +84,17 @@ int main(int argc, char **argv) {
             if (std::regex_match(info.name, topic_pattern)) {
                 subscribeOdom(info.name, nh);
                 ROS_INFO("Subscribed to %s", info.name.c_str());
+
+                // 为每个UAV创建一个独立的Publisher
+                std::string publisher_topic = info.name + "_odomWithNeighbors";  // 创建基于UAV名称的话题
+                odom_neighbors_publishers[info.name] = nh.advertise<water_swarm::OdomWithNeighbors>(publisher_topic, 1000);
+                ROS_INFO("Publisher created for topic: %s", publisher_topic.c_str());
             }
         }
     }
+
+    odomBroadcast_pub = nh.advertise<water_swarm::OdomBroadcast>("/odomBroadcast",1000);
+
 
     ros::spin();
     return 0;
