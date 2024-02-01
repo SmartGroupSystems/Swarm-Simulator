1d72b3c76eba1a8de4147751000c6ed00820507f
Ruocheng Li <1598863621@qq.com>
2024-02-01 22:22:35 +0800

    update sph

--- src/water_swarm/include/water_swarm/sph.h
+++ src/water_swarm/include/water_swarm/sph.h
@@ -4,13 +4,104 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <ros/topic_manager.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/String.h>
 #include <regex>
 #include <map>
+#include <thread>
 
+#include "water_swarm/Position.h"
+#include "water_swarm/Velocity.h"
+#include "water_swarm/Acceleration.h"
+#include "water_swarm/Force.h"
 #include "water_swarm/Odom.h"
 #include "water_swarm/OdomWithNeighbors.h"
 #include "water_swarm/OdomBroadcast.h"
 
+ros::Subscriber                         odomBroadcast_sub;
+ros::Timer                              timer;
+ros::Subscriber                         nav_goal_sub;
+std::map<std::string, ros::Subscriber>  odomSubscribers;
+
+bool isInitialReceived = false;  // 用于检查是否已经接收到第一个odomBroadcast
+water_swarm::OdomBroadcast  initial_odomBroadcast_;
+water_swarm::OdomBroadcast  current_odomBroadcast_;
+
+void odomBroadcastCallback(const water_swarm::OdomBroadcast::ConstPtr& msg);
+void navGoalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+void timerCallback(const ros::TimerEvent&);
+void subscribeOdomWithNeighbors(const std::string &topic_name, ros::NodeHandle &nh);
+void odomWithNeighborsCallback(const water_swarm::OdomWithNeighborsConstPtr& msg, const std::string& uav_name); 
+
+struct SPHSettings
+{
+    SPHSettings(
+        float mass, float restDensity, float gasConst, float viscosity,
+        float h, float g, float tension);
+
+    float poly6, spikyGrad, spikyLap, gasConstant, mass, h2, selfDens,
+        restDensity, viscosity, h, g, tension, massPoly6Product;
+};
+
+struct Particle
+{
+    water_swarm::Position       position;
+    water_swarm::Velocity       velocity;
+    water_swarm::Acceleration   acceleration;
+    water_swarm::Force          force;
+    float                       density;
+    float                       pressure;
+    uint16_t                    hash;
+};
+
+class SPHSystem
+{
+public:
+    SPHSettings     settings;
+    size_t          particleCubeWidth;
+
+    bool started;
+    bool runOnGPU;
+    bool isInitialReceived = false;  // 用于检查是否已经接收到第一个消息
+
+    //initializes the particles that will be used
+	void initParticles();
+
+public:
+	SPHSystem(
+        size_t numParticles, const SPHSettings &settings,
+        const bool &runOnGPU);
+    SPHSystem();
+	~SPHSystem();
+
+    Particle                    *particles;
+    size_t                      particleCount;
+
+    /// Finite State Machine
+    //updates the SPH system
+	void update(float deltaTime);
+	void reset();
+	void start();
+    void stop();
+
+    /// init planner
+    void initPlanner();
+
+    /// Update attrs of particles in place.
+    void updateParticles(
+        Particle *particles, const size_t particleCount, const SPHSettings &settings,
+        float deltaTime, const bool onGPU);
+
+    /// Update attrs of particles in place.
+    void updateParticlesGPU(
+        Particle *particles, const size_t particleCount, const SPHSettings &settings,
+        float deltaTime);
+
+    void updateParticlesCPU(
+        Particle *particles, const size_t particleCount, const SPHSettings &settings,
+        float deltaTime);
+    
+
+};
 
 #endif
--- src/water_swarm/src/sph.cpp
+++ src/water_swarm/src/sph.cpp
@@ -1,13 +1,182 @@
 #include "sph.h"
 
+#define PI 3.14159265f
+SPHSystem sph_planner;
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "sph_planner");
     ros::NodeHandle nh("~");
+    ros::master::V_TopicInfo master_topics;
+    ros::master::getTopics(master_topics);
+
+    //ros sub&pub
+    odomBroadcast_sub   = nh.subscribe("/odomBroadcast", 1000,  odomBroadcastCallback);
+    timer               = nh.createTimer(ros::Duration(0.05),   timerCallback);
+    nav_goal_sub        = nh.subscribe("/move_base_simple/goal", 10, navGoalCallback);
+    
+    //create parallel sub
+    for (const auto &info : master_topics) {
+            // Check if topic name matches the pattern /uav(i)/sim/odom
+            std::regex topic_pattern("/uav\\d+/sim/odom_odomWithNeighbors");
+            if (std::regex_match(info.name, topic_pattern)) {
+                subscribeOdomWithNeighbors(info.name, nh);
+            }
+            // every uav has a buffer to store the odom_withNeighbors.
+            
+    }
+
+    sph_planner.initPlanner();
 
     ROS_INFO("sph_planner node has started.");
-    
 
     ros::spin();
     return 0;
+}
+
+void odomWithNeighborsCallback(const water_swarm::OdomWithNeighborsConstPtr& msg, const std::string& uav_name) {
+
+}
+
+void subscribeOdomWithNeighbors(const std::string &topic_name, ros::NodeHandle &nh) {
+    odomSubscribers[topic_name] = nh.subscribe<water_swarm::OdomWithNeighbors>(
+        topic_name, 
+        1000, 
+        boost::bind(odomWithNeighborsCallback, _1, topic_name)
+    );
+}
+
+void navGoalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+    sph_planner.started = true;
+    ROS_INFO("Received 2D Nav Goal at position: (%.2f, %.2f, %.2f), orientation: (%.2f, %.2f, %.2f, %.2f)",
+             msg->pose.position.x, msg->pose.position.y, msg->pose.position.z,
+             msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w);
+    // 
+}
+
+void timerCallback(const ros::TimerEvent&) {
+    // ROS_INFO("Timer triggered");
+    if (!sph_planner.started) 
+    {
+        ROS_INFO("Do not receive the start command, return.");
+        return;
+    }
+    else
+    {   /// Here deltaTime is no use, just satisfy the func...
+        float deltaTime;
+        sph_planner.update(deltaTime);
+    }
+}
+
+void odomBroadcastCallback(const water_swarm::OdomBroadcast::ConstPtr& msg) {
+    if (!isInitialReceived) {
+        initial_odomBroadcast_ = *msg;
+        isInitialReceived = true;
+        // ROS_INFO("Initial OdomBroadcast received.");
+    } else {
+        current_odomBroadcast_ = *msg;
+        // ROS_INFO("Current OdomBroadcast updated.");
+    }
+}
+
+SPHSettings::SPHSettings(
+    float mass, float restDensity, float gasConst, float viscosity, float h,
+    float g, float tension)
+    : mass(mass)
+    , restDensity(restDensity)
+    , gasConstant(gasConst)
+    , viscosity(viscosity)
+    , h(h)
+    , g(g)
+    , tension(tension)
+{
+    poly6 = 315.0f / (64.0f * PI * pow(h, 9));
+    spikyGrad = -45.0f / (PI * pow(h, 6));
+    spikyLap = 45.0f / (PI * pow(h, 6));
+    h2 = h * h;
+    selfDens = mass * poly6 * pow(h, 6);
+    massPoly6Product = mass * poly6;
+}
+
+SPHSystem::SPHSystem(
+    size_t particleCubeWidth, const SPHSettings &settings,
+    const bool &runOnGPU)
+    : particleCubeWidth(particleCubeWidth)
+    , settings(settings)
+    , runOnGPU(runOnGPU)
+{
+    // particleCount = particleCubeWidth * particleCubeWidth * particleCubeWidth;
+    particleCount = initial_odomBroadcast_.OdomBroadcast.size();
+    particles = (Particle*)malloc(sizeof(Particle) * particleCount);
+
+    initParticles();
+
+	//start init
+	started = false;
+}
+
+SPHSystem::~SPHSystem() {
+    delete[](particles);
+}
+
+void SPHSystem::initPlanner()
+{
+    // particleCount = particleCubeWidth * particleCubeWidth * particleCubeWidth;
+    particleCount = initial_odomBroadcast_.OdomBroadcast.size();
+    particles = (Particle*)malloc(sizeof(Particle) * particleCount);
+
+    initParticles();
+
+	//start init
+	started = false;
+}
+
+void SPHSystem::initParticles()
+{
+    for (size_t i = 0; i < particleCount; i++)
+    {
+        Particle* particle = &particles[i];
+        particle->position = initial_odomBroadcast_.OdomBroadcast[i].position;
+        particle->velocity = initial_odomBroadcast_.OdomBroadcast[i].velocity;
+    }
+}
+
+void SPHSystem::update(float deltaTime) {
+	if (!started) return;
+	// To increase system stability, a fixed deltaTime is set
+	deltaTime = 0.003f;
+    runOnGPU = false;
+    updateParticles(particles, particleCount, settings, deltaTime,runOnGPU);
+}
+
+void SPHSystem::reset() {
+    isInitialReceived = false;
+	initParticles();
+	started = false;
+}
+
+void SPHSystem::start() {
+	started = true;
+}
+
+void SPHSystem::updateParticles(
+    Particle *particles, const size_t particleCount, const SPHSettings &settings,
+    float deltaTime, const bool onGPU)
+{
+    if (onGPU) {
+        updateParticlesGPU(
+            particles, particleCount, settings, deltaTime);
+    }
+    else {
+        updateParticlesCPU(
+            particles, particleCount, settings, deltaTime);
+    }
+}
+
+/// CPU update particles implementation
+void SPHSystem::updateParticlesCPU(
+    Particle *particles, const size_t particleCount, const SPHSettings &settings,
+    float deltaTime)
+{
+
+
 }
